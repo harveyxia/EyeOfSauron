8bf36b2cb9727ac92943a91dab3540570deec3f8
Harvey Xia <harvey@harveyxia.com>
2014-04-26 00:07:20 -0400

    begin transcribing from bash version

--- EyeOfSauron.py
+++ EyeOfSauron.py
@@ -1,16 +1,36 @@
 import subprocess
 import ldap
 
-l = ldap.open("directory.yale.edu")
-l.simple_bind("", "")
+nodeList = [ "aphid", "lion", "macaw", "bumblebee", "monkey", "cardinal",
+             "newt", "chameleon", "peacock", "cicada", "perch", "cobra",
+             "python", "cricket", "dolphin", "rhino", "frog", "scorpion",
+             "swan", "gator", "termite", "giraffe", "tick", "grizzly", "tiger",
+             "hare", "turtle", "hippo", "viper", "hornet", "jaguar", "zebra",
+             "kangaroo", "ladybug"]
+
+def main():
+    # open ldap connection
+    l = ldap.open("directory.yale.edu")
+    l.simple_bind("", "")
+
+    zoo_users = traverse_zoo()
+
+    l.unbind_s()
+
+def traverse_zoo():
+    zoo_users = []
+    for node in nodeList:
+        users = subprocess.check_output(['ssh', 'hx52@' + node, 'users'])
+        zoo_users.append(users.strip('\n').split(' '))
+    return zoo_users
 
 def search_ldap(netid):
-    baseDN = 'ou=People,o=yale.edu'
-    searchScope = ldap.SCOPE_SUBTREE
+    base = 'ou=People,o=yale.edu'
+    scope = ldap.SCOPE_SUBTREE
     retrieveAttributes = None
-    searchFilter = netid
+    sfilter = netid
 
-    ldap_result_id = l.search(baseDN, searchScope, searchFilter, retrieveAttributes)
+    ldap_result_id = l.search(base, scope, sfilter, retrieveAttributes)
     result_set = []
     while 1:
             result_type, result_data = l.result(ldap_result_id, 0)
@@ -23,6 +43,8 @@ def search_ldap(netid):
                     if result_type == ldap.RES_SEARCH_ENTRY:
                             result_set.append(result_data)
     print result_set
+    
     return result_set
 
-l.unbind_s()
+if __name__ == "__main__":
+    main()
